714f0bdadea84914e8f2958859b7035304757e5c
suiramchi-blip <suiramchi@gmail.com>
2026-09-24 15:45:13 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3882,7 +3882,7 @@ const orderedChordsForStrip = useMemo(() => {
     </div>
   </div>
 )}
-      {!selectedSong && !showCategories ? (
+      {!selectedSong ? (
         <div style={{ maxWidth: 720, margin: "0 auto", position: "relative", zIndex: 1 }}>
         
           <div
